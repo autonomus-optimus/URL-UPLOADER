be39c21425b30a44697abb977f1fc309315352f4
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2023-04-07 20:01:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from pyrogram import Client
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,15 +7,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    
-
-    API_ID = 11652964
-    API_HASH = "0123456789abcdef0123456789abcdef"
-    BOT_TOKEN = "6152226851:AAGRUrOephKbiBbNrJussdt-oFTDMWTZ_Sk"
-
-    app = Client(
-        "my_bot",
-        API_ID=API_ID, API_HASH=API_HASH,
-        BOT_TOKEN=BOT_TOKEN
-    )
     app.run()
